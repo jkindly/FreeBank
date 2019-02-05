e8617b61ba114a3b7c1810c7322256704f1690f1
jkindly <kubakus222@gmail.com>
2019-02-05 22:33:10 +0100

    creating random bank accounts

--- src/DataFixtures/BankAccountFixtures.php
+++ src/DataFixtures/BankAccountFixtures.php
@@ -10,11 +10,27 @@ class BankAccountFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-//         $bankAccount = new BankAccount();
-//         $bankAccount->setAccountType('normal')
-//             ->setAccountNumber()
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $bankAccount = new BankAccount();
+
+            $bankAccount
+                ->setAccountType('standard')
+                ->setAccountNumber($this->randomAccountNumber())
+                ->setBalance(0.00)
+                ->setAvailableFunds(0.00);
+            $manager->persist($bankAccount);
+        }
 
         $manager->flush();
+    }
+
+    private function randomAccountNumber() {
+        $controlSum    = 77;
+        $billingNumber = 11402004;
+        $restNumber    = rand(1000000000000000, 9999999999999999);
+
+        $accountNumber = (string)$controlSum . (string)$billingNumber . (string)$restNumber;
+
+        return $accountNumber;
     }
 }
